91bb176447ac81060e00820a8f7240317ded119b
Reminisce <euniceho1111@gmail.com>
2026-01-20 08:54:57 +0800

    Update DAY04_TASK2.py

--- DAY04_TASK2.py
+++ DAY04_TASK2.py
@@ -1,1 +1,12 @@
 
+try: 
+	a = int(input("Input an integer"))
+	b = int(input("Input another integer"))
+  
+	result = a/b
+except ZeroDivisionError:
+	print("cannot divide by zero")
+except ValueError:
+	print("not a number")
+  
+  
