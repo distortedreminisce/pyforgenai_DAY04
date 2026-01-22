071fce5a2c0dade2966a2eda470225ab720a7c6f
Reminisce <euniceho1111@gmail.com>
2026-01-22 14:41:45 +0800

    Update api_fetcher.py

--- api_fetcher.py
+++ api_fetcher.py
@@ -2,12 +2,12 @@ import requests
 import json 
 
 def menu():
-  print("Menu:")
-  print("1. View all users")
-  print("2. View selected user")
-  print("3. View selected user's posts")
-  print("4. Exit")
-  choice = int(input("Input your choice: "))
+	print("Menu:")
+	print("1. View all users")
+	print("2. View selected user")
+	print("3. View selected user's posts")
+	print("4. Exit")
+	choice = int(input("Input your choice: "))
 
 print("Welcome to reminisce's ali fetcher!")
 
